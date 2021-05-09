5ee9ece395f2877f35baac1f1b930597cc289721
Chris Dostert <chrisdostert@users.noreply.github.com>
2021-05-09 13:54:05 -0700

    This works with razzle 4+

--- index.js
+++ index.js
@@ -1,28 +1,28 @@
-"use strict";
+const loaderFinder = require('razzle-dev-utils/makeLoaderFinder')
 
-const loaderFinder = require('razzle-dev-utils/makeLoaderFinder');
-
-module.exports = function modify(baseConfig) {
-  const config = Object.assign({}, baseConfig);
-
-  const babelLoaderConfig = loaderFinder('babel-loader');
-  const javascriptRule = config.module.rules.find(babelLoaderConfig).use.find(babelLoaderConfig).options;
-  const svgConfig = [
-    require.resolve('babel-plugin-named-asset-import'),
-    {
-      loaderMap: {
-        svg: {
-          ReactComponent: '@svgr/webpack?-svgo,+titleProp,+ref![path]',
-        },
-      },
-    },
-  ]
-
-  if (javascriptRule.plugins) {
-    javascriptRule.plugins.push(svgConfig);
-  } else {
-    javascriptRule.plugins = [svgConfig];
+module.exports = {
+    modifyWebpackConfig({
+      webpackConfig
+    }) {
+        const babelLoaderConfig = loaderFinder('babel-loader')
+        const javascriptRule = webpackConfig.module.rules.find(babelLoaderConfig).use.find(babelLoaderConfig).options
+        const svgConfig = [
+          require.resolve('babel-plugin-named-asset-import'),
+          {
+            loaderMap: {
+              svg: {
+                ReactComponent: '@svgr/webpack?-svgo,+titleProp,+ref![path]',
+              }
+            }
+          }
+        ]
+      
+        if (javascriptRule.plugins) {
+          javascriptRule.plugins.push(svgConfig)
+        } else {
+          javascriptRule.plugins = [svgConfig]
+        }
+      
+        return webpackConfig
+    }
   }
-
-  return config;
-};
